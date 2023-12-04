08ab3045afd81700d024808d6ebdc12944734d1f
Elliot Nykvist <elnv23@student.bth.se>
2023-12-04 14:03:36 +0100

    added text over the video

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -2,15 +2,22 @@ import "./Video.css";
 import HoverVideoPlayer from 'react-hover-video-player';
 
 const Video = () => {
-  return (  
-    <HoverVideoPlayer
-    videoSrc="/volvo.mp4"
-    pausedOverlay={ 
-      <img
-        src="thumbnail-image.jpg"
-        alt=""
-      /> }
-    />
+  return ( 
+    <>
+      <HoverVideoPlayer className="video"
+      videoSrc="/volvo.mp4"
+      pausedOverlay={ 
+        <img
+          src="thumbnail-image.jpg"
+          alt=""
+        /> }
+      />
+      <div className="text">
+        <h1>Combine & save with <br></br>Volvo Offers</h1>
+        <p>Maximise your savings now by combining your exclusive offers.</p>
+        <button>View Offers</button>
+      </div>
+    </>
   )
 }
 
